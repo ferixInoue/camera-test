8165c09a13b715fef27aecbe096ee69d126824cb
inoue12 <inoue@ferix.jp>
2020-08-12 16:58:47 +0900

    init

---
+++ components/CameraButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <a class="photo-button">
+    <div class="circle"></div>
+    <div class="ring"></div>
+  </a>
+</template>
+<script lang="ts">
+import { ref } from '@vue/composition-api'
+export default {
+  name: '',
+  setup() {
+    return {}
+  },
+}
+</script>
+<style>
+.photo-button {
+  width: 100px;
+  height: 100px;
+  position: absolute;
+}
+.circle {
+  position: absolute;
+  top: 12%;
+  left: 12%;
+  bottom: 12%;
+  right: 12%;
+  border-radius: 100%;
+  background-color: rgb(248, 199, 199);
+  opacity: 0;
+}
+.ring {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  border-radius: 100%;
+  border: 0.5em solid rgb(240, 217, 217);
+  opacity: 0.8;
+}
+.photo-button .circle,
+.photo-button .ring {
+  transition: all 0.25s;
+}
+.photo-button:hover .circle {
+  opacity: 1;
+}
+.photo-button:active .ring {
+  opacity: 1;
+}
+.photo-button:active .circle {
+  opacity: 0.5;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>カメラテスト</h1>
+      <video id="video" ref="video" width="100%" height="500" autoplay></video>
+      <div>
+        <button
+          id="snap"
+          class="px-3 py-2 bg-blue-500 text-white"
+          @click="capture()"
+        >
+          Snap Photo
+        </button>
+      </div>
+      <canvas id="canvas" ref="canvas"></canvas>
+      <ul>
+        <li
+          v-for="c in captures"
+          :key="c.d"
+          class="capture flex flex-col items-center"
+        >
+          <img :src="c" />
+          Base64 Encoded:
+          <textarea
+            :value="c"
+            style="width: 400px; height: 100px; resize: both;"
+          ></textarea>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, onMounted } from '@vue/composition-api'
+export default {
+  setup(_props: {}, { refs }: any) {
+    const video = ref<HTMLVideoElement>(document.createElement('video'))
+    const canvas = ref<HTMLCanvasElement>(document.createElement('canvas'))
+    const captures = ref<Array<string>>([])
+    const width = ref(500)
+    const height = ref(500)
+    const ctx = ref<CanvasRenderingContext2D | null>(null)
+
+    const constraints = {
+      audio: false,
+      video: { facingMode: 'environment' },
+    }
+
+    onMounted(() => {
+      video.value = refs.video
+
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia(constraints).then((stream: any) => {
+          video.value.srcObject = stream
+          video.value.play()
+        })
+      }
+    })
+
+    /**
+     * Canvasオブジェクト取得
+     */
+    const getCanvas = (
+      canvasId: string,
+      width: number,
+      hegiht: number
+    ): HTMLCanvasElement => {
+      const _canvas = document.createElement(canvasId) as HTMLCanvasElement
+      _canvas.height = hegiht
+      _canvas.width = width
+      return _canvas
+    }
+
+    const capture = () => {
+      if (!ctx.value) {
+        canvas.value = getCanvas(
+          'canvas',
+          video.value.videoWidth,
+          video.value.videoHeight
+        )
+        ctx.value = canvas.value.getContext('2d') as CanvasRenderingContext2D
+      }
+      if (!canvas.value) return
+      ctx.value.drawImage(
+        video.value,
+        0,
+        0,
+        video.value.videoWidth,
+        video.value.videoHeight
+      )
+      captures.value.unshift(canvas.value.toDataURL('image/jpeg'))
+    }
+    return { capture, captures, height, width }
+  },
+}
+</script>
+
+<style>
+#canvas {
+  display: none;
+}
+#video {
+  background-color: #000;
+}
+html,
+body {
+  font-family: 'Noto Sans JP', sans-serif;
+  font-weight: 400;
+}
+h1 {
+  font-weight: 800;
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: Nato san JP, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+</style>
