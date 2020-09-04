5b2c4365c8d8109cdf286e4e544c0472b7800b6e
inoue12 <inoue@ferix.jp>
2020-09-04 16:54:55 +0900

    アップロードロジック変更

--- components/FileUploader.vue
+++ components/FileUploader.vue
@@ -1,52 +1,50 @@
 <template>
-  <div>
+<div>
     <TheLoading v-if="isLoading" />
-    {{ file }}
-    <input
-      :value="file"
-      type="file"
-      accept="image/*"
-      class="border-gray-300 p-3"
-      @change="onFileChange($event)"
-    />
-  </div>
+    <input type="file" accept="image/*" class="border-gray-300 p-3" @change="onFileChange($event)" />
+</div>
 </template>
+
 <script lang="ts">
-import { ref, defineComponent, SetupContext, watch } from '@vue/composition-api'
+import {
+    ref,
+    defineComponent,
+    SetupContext,
+    watch
+} from '@vue/composition-api'
 export default defineComponent({
-  name: '',
-  setup(_props: {}, { emit }: SetupContext) {
-    const imageData = ref('')
-    const isLoading = ref(false)
-    const file = ref<any>('')
-    const onFileChange = (e: any) => {
-      isLoading.value = true
-      const files = e.target.files
+    name: '',
+    setup(_props: {}, {
+        emit
+    }: SetupContext) {
+        const imageData = ref('')
+        const isLoading = ref(false)
+        const onFileChange = (e: any) => {
+            isLoading.value = true
+            const files = e.target.files
 
-      if (files.length > 0) {
-        const file = files[0]
-        const reader = new FileReader()
-        reader.onload = (e: any) => {
-          imageData.value = e.target.result
-          isLoading.value = false
+            if (files.length > 0) {
+                const file = files[0]
+                const reader = new FileReader()
+                reader.onload = (e: any) => {
+                    imageData.value = e.target.result
+                    isLoading.value = false
+                }
+                reader.readAsDataURL(file)
+            }
         }
-        reader.readAsDataURL(file)
-        //
-      }
-    }
-    watch(
-      () => imageData.value,
-      () => {
-        emit('onImagePushed', imageData.value)
-      }
-    )
+        watch(
+            () => imageData.value,
+            () => {
+                emit('onImagePushed', imageData.value)
+            }
+        )
 
-    return {
-      onFileChange,
-      imageData,
-      isLoading,
-      file,
-    }
-  },
+        return {
+            onFileChange,
+            imageData,
+            isLoading,
+        }
+    },
 })
 </script>
